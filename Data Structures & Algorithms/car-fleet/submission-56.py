cb74b0ae1297638a072ebbd203d93d622d7ad28e
Miles Fritzmather <90790095+Miles-Fritzmather@users.noreply.github.com>
2026-07-21 00:14:21 -0500

    Add: car-fleet - submission-56

---
+++ Data Structures & Algorithms/car-fleet/submission-56.py
@@ -0,0 +1,16 @@
+import math
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        comb = [(position[i], speed[i], (target - position[i]) / speed[i]) for i in range(len(speed))]
+        comb.sort(key=lambda a: a[0])
+        
+        clusters = 1
+
+        slowest = comb[-1][2]
+        for i in range(len(comb) - 2, -1, -1):
+            if comb[i][2] > slowest:
+                slowest = comb[i][2]
+                clusters += 1
+
+        return clusters
