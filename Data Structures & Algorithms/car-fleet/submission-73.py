b3006992280318bc5e7554fa30f9629d7f39211c
Miles Fritzmather <90790095+Miles-Fritzmather@users.noreply.github.com>
2026-07-21 00:22:19 -0500

    Add: car-fleet - submission-73

---
+++ Data Structures & Algorithms/car-fleet/submission-73.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(position[i], (target - position[i]) / speed[i]) for i in range(len(speed))]
+        pairs.sort(key=lambda pair: -pair[0])
+        
+        clusters = 1
+        slowest = pairs[0][1]
+        for i in range(0, len(pairs)):
+            time = pairs[i][1]
+            
+            if time > slowest: clusters += 1
+            slowest = max(slowest, time)
+
+        print(pairs)
+        pairs.sort(key=lambda pair: -pair[0])
+        print(pairs)
+        pairs.sort(key=lambda pair: pair[0])
+        pairs.reverse()
+        print(pairs)
+
+        return clusters
