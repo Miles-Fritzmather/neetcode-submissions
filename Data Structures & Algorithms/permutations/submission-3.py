2ead3491b29d8e1ab266902d1759934d895ffc06
Miles Fritzmather <90790095+Miles-Fritzmather@users.noreply.github.com>
2026-08-08 22:27:32 -0500

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1: return [nums]
+
+        perms = self.permute(nums[1:])
+        res = []
+
+        for perm in perms:
+            for i in range(len(perm) + 1):
+                res.append(perm[0:i] + [nums[0]] + perm[i:])
+
+        return res
+
+
+    
+    # def permute(self, nums: List[int]) -> List[List[int]]:
+    #     perms = [[n] for n in nums]
+
+    #     def increment(current: List[List[int]]):
+    #         new = []
+    #         for perm in current:
+    #             for num in nums:
+    #                 if num in perm: continue
+    #                 new.append(perm + [num])
+    #         return new
+
+
+    #     for i in range(1, len(nums)):
+    #         perms = increment(perms)
+
+    #     return perms
