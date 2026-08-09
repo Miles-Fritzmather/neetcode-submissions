479d135b36608b15e371b3dd7f29c53c65c9c619
Miles Fritzmather <90790095+Miles-Fritzmather@users.noreply.github.com>
2026-08-09 01:38:15 -0500

    Add: word-ladder - submission-16

---
+++ Data Structures & Algorithms/word-ladder/submission-16.py
@@ -0,0 +1,22 @@
+class Solution:
+    def ladderLength(self, beginWord: str, goal: str, wordList: List[str]) -> int:
+        def oneAway(a: str, b:str) -> bool:
+            missed = False
+            for i in range(len(a)):
+                if (a[i] != b[i]):
+                    if missed: return False
+                    missed = True
+            return True
+
+        options = deque([(beginWord, 1)])
+        path = set([beginWord])
+        while options:
+            word, count = options.popleft()
+            print(options, path, word)
+            for option in wordList:
+                if oneAway(option, word) and option not in path:
+                    if option == goal: return count + 1
+                    path.add(option)
+                    options.append((option, count + 1))
+
+        return 0
