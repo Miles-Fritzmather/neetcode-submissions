2ac8a776412cbaca42d6959d695042a0e65b4a9a
Miles Fritzmather <90790095+Miles-Fritzmather@users.noreply.github.com>
2026-09-11 20:50:55 -0500

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        def cind(char):
+            return ord(char.lower()) - ord('a') 
+        
+        
+        if len(s1) > len(s2): return False
+        
+        small_counts = [0] * 26
+        for c in s1: small_counts[cind(c)] += 1
+        
+        counts = [0] * 26
+        for i in range(0, len(s1)): counts[cind(s2[i])] += 1
+        if counts == small_counts: return True
+
+        for i in range(0, len(s2) - len(s1)):
+            counts[cind(s2[i])] = max(0, counts[cind(s2[i])] - 1)
+            counts[cind(s2[i + len(s1)])] += 1
+            if counts == small_counts: return True
+        
+
+        return False
